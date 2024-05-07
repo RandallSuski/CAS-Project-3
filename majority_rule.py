6198ec595f035cdbbaf07aca5ba474970d07415c
Randall Suski-Oppedahl <randallsuski@unm.edu>
2024-05-06 22:32:20 -0600

    Adding data collected from an improved majority_rule.py

--- majority_rule.py
+++ majority_rule.py
@@ -1,10 +1,11 @@
 import random
 import matplotlib.pyplot as plt
 
+
 # This is the function that will determine the next state of the cell given a rule string
 # With the two ending input cells being randomly chosen from the population rather than a neighborhood
 def next_cell_wireless(current_state, rule_string, index, length, ca_radius):
-    neighborhood_radius = ca_radius - 2
+    neighborhood_radius = ca_radius - 1
     rule_index = 0
     # The first bit is randomly chosen from the whole sample
     if current_state[random.randint(0, len(current_state) - 1)] == 1:
@@ -23,10 +24,29 @@ def next_cell_wireless(current_state, rule_string, index, length, ca_radius):
         if current_state[current_index] == 1:
             rule_index += 1
         rule_index = rule_index << 1
+        current_index += 1
 
     # Final bit is also randomly chosen
     if current_state[random.randint(0, len(current_state) - 1)] == 1:
         rule_index += 1
+    return rule_string[rule_index]
+
+
+def next_cell_neighborhood(current_state, rule_string, index, length, ca_radius):
+    rule_index = 0
+    current_index = index - ca_radius
+    for i in range(ca_radius * 2 + 1):
+        # Check for valid index
+        if current_index >= length:
+            current_index -= length
+        if current_index < 0:
+            current_index += length
+
+        if current_state[current_index] == 1:
+            rule_index += 1
+        if i != ca_radius * 2:
+            rule_index = rule_index << 1
+        current_index += 1
     return rule_string[rule_index]
 
 
@@ -47,7 +67,7 @@ def create_IC(num_ones, IC_size):
     return IC
 
 
-def test_CA(IC, num_ones, rule, ca_radius):
+def test_CA(IC, num_ones, rule, ca_radius, next_cell_function):
     # Determine if the IC converges to 1 or 0
     converge_value = 0
     if num_ones / len(IC) > 0.5:
@@ -55,7 +75,7 @@ def test_CA(IC, num_ones, rule, ca_radius):
     # Run through 200 time steps
     state = IC
     for _ in range(200):
-        state = next_state(state, rule, ca_radius)
+        state = next_state(state, rule, ca_radius, next_cell_function)
     # If after 200 steps the state converged to one value and that value is correct, return 1 for fitness
     if is_state_converged(state) and state[0] == converge_value:
         return 1
@@ -71,10 +91,10 @@ def is_state_converged(state):
     return True
 
 
-def next_state(old_state, rule, ca_radius):
+def next_state(old_state, rule, ca_radius, next_cell_function):
     new_state = [0] * len(old_state)
     for cell_index in range(len(new_state)):
-        if next_cell_wireless(old_state, rule, cell_index, len(old_state), ca_radius) == '0':
+        if next_cell_function(old_state, rule, cell_index, len(old_state), ca_radius) == '0':
             new_state[cell_index] = 0
         else:
             new_state[cell_index] = 1
@@ -83,28 +103,36 @@ def next_state(old_state, rule, ca_radius):
 
 if __name__ == "__main__":
     majority_rule = "00000000000000010000000100010111000000010001011100010111011111110000000100010111000101110111111100010111011111110111111111111111"
+    GKL_rule = "00000101000001100001010110000111000001110000010000010101010101110110010001110111000001010000000101111101111111111011011101111111"
     lattice_length = 199
     radius = 3
 
     # Want to create 100 starting IC's for each rho = 0/lattice_length - lattice_length / lattice_length
     # Then run the rule on each one, calculating the fitness and storing the fitness for a particular rho
     rho_values = []
-    fitness_values = []
+    fitness_values_wireless = []
+    fitness_values_neighborhood = []
     for i in range(lattice_length + 1):
         # i represents number of 1's
         rho_values.append(i / lattice_length)
-        fitness_score = 0
+        fitness_score_wireless = 0
+        fitness_score_neighborhood = 0
         # Test 100 IC's with i number of 1's
-        for j in range(100):
+        for _ in range(100):
             # Create an IC with i number of 1's
             my_IC = create_IC(i, lattice_length)
-            fitness_score += test_CA(my_IC, i, majority_rule, radius)
-        fitness_values.append(fitness_score)
-        print("Fitness Score for " + str(i) + " number of ones: " + str(fitness_score))
+            fitness_score_wireless += test_CA(my_IC, i, majority_rule, radius, next_cell_wireless)
+            fitness_score_neighborhood += test_CA(my_IC, i, GKL_rule, radius, next_cell_neighborhood)
+        fitness_values_wireless.append(fitness_score_wireless / 100)
+        fitness_values_neighborhood.append(fitness_score_neighborhood / 100)
+        print(i)
 
     # Plot the rho values and fitness scores
-    plt.plot(rho_values, fitness_values)
+    plt.plot(rho_values, fitness_values_wireless, label="Wireless Majority Rule")
+    plt.plot(rho_values, fitness_values_neighborhood, label="Neighborhood GKL Rule")
+    plt.ylim(0, 1)
     plt.xlabel('Rho-Values')
     plt.ylabel('Fitness')
-    plt.title('Fitness of the Majority Rules Rule for a wireless CA')
+    plt.legend(title="Rule and Way of Gathering Information", labels=['Wireless Majority', 'GKL Neighborhood'])
+    plt.title('Majority Rule w/wireless vs. Standard GKL Rule')
     plt.show()
